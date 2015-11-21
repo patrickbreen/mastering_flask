e44ee87a316ae5f3720b3ee6c69d0ca82d5ccb8a
patrick <pbreen@uga.edu>
2015-11-21 17:36:35 -0500

    add home template, fake data and pagination, and views

---
+++ fake_data_gen.py
@@ -0,0 +1,24 @@
+import random
+import datetime
+from main import app, db, User, Post, Tag, Comment
+
+user = User(username="jim", password="jim")
+db.session.add(user)
+
+tag_one = Tag(title='Python')
+tag_two = Tag(title='Flask')
+tag_three = Tag(title='SQLAlchemy')
+tag_four = Tag(title='Jinja')
+tag_list = [tag_one, tag_two, tag_three, tag_four]
+s = "Example text"
+
+
+for i in range(100):
+    new_post = Post(title="Post " + str(i))
+    new_post.user = user
+    new_post.publish_date = datetime.datetime.now()
+    new_post.text = s
+    new_post.tags = random.sample(tag_list, random.randint(1,3))
+    db.session.add(new_post)
+
+db.session.commit()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 from config import DevConfig
 
@@ -18,6 +19,11 @@ class User(db.Model):
         lazy='dynamic'
     )
 
+tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    )
+
 
 class Post(db.Model):
   id = db.Column(db.Integer(), primary_key=True)
@@ -32,8 +38,8 @@ class Post(db.Model):
   user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
   tags = db.relationship(
       'Tag',
-      backref='post',
-      lazy='dynamic'
+      secondary=tags,
+      backref=db.backref('posts', lazy='dynamic')
   )
 
 
@@ -50,10 +56,72 @@ class Comment(db.Model):
   post_id = db.Column(db.Integer(),
       db.ForeignKey('post.id'))
 
+def sidebar_data():
+    recent = Post.query.order_by(
+        Post.publish_date.desc()).limit(5).all()
+    top_tags = db.session.query(
+        Tag, func.count(tags.c.post_id).label('total')
+        ).join(
+            tags
+        ).group_by(Tag).order_by('total DESC').limit(5).all()
+    return recent, top_tags
+
 
 @app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
+@app.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(
+        Post.publish_date.desc()
+        ).paginate(page, 10)
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'home.html',
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+     )
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    tags = post.tags
+    comments = post.comments.order_by(Comment.data.desc()).all()
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'post.html',
+        post=post,
+        tags=tags,
+        comments=comments,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+@app.route('/tag/<string:tag_name>')
+def tag(tag_name):
+    tag = Tag.query.filter_by(title-tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'tag.html',
+        tag=tag,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+@app.route('/user/<string:username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
